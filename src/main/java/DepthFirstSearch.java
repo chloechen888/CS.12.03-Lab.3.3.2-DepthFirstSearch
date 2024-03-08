c344122123bf00c33620a5b7ebea27ee46cd3dcf
chloechen <tszkc2024@student.cis.edu.hk>
2024-03-08 15:13:19 +0800

    hI G

--- src/main/java/DepthFirstSearch.java
+++ src/main/java/DepthFirstSearch.java
@@ -6,33 +6,33 @@ public class DepthFirstSearch {
     public static void main(String[] args) {
 
         int[][] graph1 = {
-                {6,9},
+                {6, 9},
                 {11},
-                {6,7},
+                {6, 7},
                 {9},
-                {5,8},
-                {4,7},
-                {0,2,11},
-                {2,5},
-                {4,10},
-                {0,3},
+                {5, 8},
+                {4, 7},
+                {0, 2, 11},
+                {2, 5},
+                {4, 10},
+                {0, 3},
                 {8},
-                {1,6}
+                {1, 6}
         };
 
         int[][] graph2 = {
-                {3,6,5,9},
+                {3, 6, 5, 9},
                 {11},
-                {7,10},
+                {7, 10},
                 {0},
                 {5},
-                {0,4,7},
-                {0,11},
-                {2,5,8},
+                {0, 4, 7},
+                {0, 11},
+                {2, 5, 8},
                 {7},
                 {0},
                 {2},
-                {1,6}
+                {1, 6}
         };
 
         int[] path_graph_1 = depthFirstSearch(graph1);
@@ -44,50 +44,41 @@ public class DepthFirstSearch {
     }
 
     public static int[] depthFirstSearch(int[][] graph) {
+        int numNodes = graph.length;
+        boolean[] visited = new boolean[numNodes];
+        int[] path = new int[numNodes];
+        int pathIndex = 0;
 
-        // Create a stack.
+        Stack<Integer> stack = new Stack<>();
+        int startingNode = 0; // Start the traversal at node 0
+        stack.push(startingNode);
+        visited[startingNode] = true;
 
-        // Create an array called visited. This will keep track of which nodes we have visited.
+        while (!stack.isEmpty()) {
+            int currentNode = stack.pop();
+            path[pathIndex++] = currentNode;
 
-        // Create an array called path. This will keep track of the order of nodes that we visit.
+            int[] neighbors = graph[currentNode];
+            for (int neighbor : neighbors) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    stack.push(neighbor);
+                }
+            }
+        }
 
-        // Create an index for the path array.
+        // Trim the path array to remove unused elements
+        int[] trimmedPath = new int[pathIndex];
+        System.arraycopy(path, 0, trimmedPath, 0, pathIndex);
 
-        // Push our starting node to the stack. We can begin our traversal from any valid node. Let's begin our traversal at node 0.
-
-        // Record the starting node as visited.
-
-        // While our stack is not empty i.e. while we still have nodes to explore ...
-
-            // Pop the node that we are currently visiting from the stack.
-
-            // Add the node that we are currently visiting to the path.
-
-            // Obtain an array of all neighbouring/adjacent nodes of the node that we are currently visiting.
-
-            // For each neighbouring/adjacent node ...
-
-                // If the neighbouring/adjacent node has not been visited ...
-
-                    // Record the neighbouring/adjacent node as visited.
-
-                    // Push the neighbouring/adjacent node onto the stack.
-
-
-
-
-        // Return the path.
-        return null;
-
+        return trimmedPath;
     }
 
-    // A private helper method that prints the path. Used for visualisation and debugging purposes.
+    // A private helper method that prints the path. Used for visualization and debugging purposes.
     private static void printPath(int[] path) {
-
-        for (int node: path) System.out.print(node + " ");
+        for (int node : path) {
+            System.out.print(node + " ");
+        }
         System.out.println();
-
     }
-
-
 }
